445b56c02676e33d698a1e7598af7a13ab68be8e
Jose <jrecalde_98@hotmail.com>
2023-04-10 21:44:05 +0100

    display edit

--- main.js
+++ main.js
@@ -2,20 +2,30 @@ import { characters } from "./data.js";
 
 const container = document.querySelector(".card-container");
 
-
-
 characters.forEach((rowling) => {
-  const { name, alternate_names, species, gender,  house, dateOfBirth, yearOfBirth, wizard, ancestry, } = rowling;
+  const {
+    name,
+    alternate_names,
+    species,
+    house,
+    image,
+  } = rowling;
 
-  container.innerHTML += `<div> 
-     /* <img src="${Image}" alt="wizmugpic"></img> 
-      <h2>${name}</h2>
-      <p> ${species}</p>    
+  container.innerHTML += `<div class="card"> 
+     <img class="card__image" src="${image}" alt="wizmugpic"></img> 
+     <section class="card__content">
+     <h2 class="card__heading" >${name}</h2>
+     <div class="card__text">
+     if(alternate_names != 0)
+     <div>  aka <p>${alternate_names}</p> </div>
+      
+      <p> ${house}</p>    
+      </div>
+      </section>
+     
       
     </div>`;
-    console.log(species)
-container.style.backgroundColor="pink";
-
-
+  console.log(species);
+  container.style.backgroundColor = "pink";
 });
-Footer
+Footer;
