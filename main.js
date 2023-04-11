dedd08e0083c378611a88f5f6ecac5ebcd98761d
Jose <jrecalde_98@hotmail.com>
2023-04-11 02:32:05 +0100

    added searchbox

--- main.js
+++ main.js
@@ -11,6 +11,7 @@ characters.forEach((rowling) => {
     image,
   } = rowling;
 
+
   container.innerHTML += `<div class="card"> 
      <img class="card__image" src="${image}" alt="wizmugpic"></img> 
      <section class="card__content">
@@ -24,9 +25,31 @@ characters.forEach((rowling) => {
      
       
     </div>`;
+
+   
   console.log(species);
   container.style.backgroundColor = "pink";
 
-  const result = characters.filter(character => character.length > 6);
+  function liveSearch() {
+    // Locate the card elements
+    let cards = document.querySelectorAll('.card-container')
+    // Locate the search input
+    let search_query = document.getElementById("searchbox").value;
+    // Loop through the cards
+    for (var i = 0; i < cards.length; i++) {
+      // If the text is within the card...
+      if(cards[i].innerText.toLowerCase()
+        // ...and the text matches the search query...
+        .includes(search_query.toLowerCase())) {
+          // ...remove the `.is-hidden` class.
+          cards[i].classList.remove("is-hidden");
+      } else {
+        // Otherwise, add the class.
+        cards[i].classList.add("is-hidden");
+      }
+    }
+  }
+
+  
 });
 Footer;
